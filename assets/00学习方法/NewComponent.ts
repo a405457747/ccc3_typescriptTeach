52eaa2063798fd8c8da196008892675d54585eb2
Sky.Allen <894982165@qq.com>
2024-05-10 08:31:50 +0800

    3

--- assets/00学习方法/NewComponent.ts
+++ assets/00学习方法/NewComponent.ts
@@ -1,5 +1,5 @@
-import { _decorator, Color, Component, Node,tween,UIOpacity, Vec3,Label,Sprite, CCInteger, Animation} from 'cc';
-const { ccclass, property ,requireComponent,menu,executionOrder} = _decorator;
+import { _decorator, Color, Component, Node, tween, UIOpacity, Vec3, Label, Sprite, CCInteger, Animation } from 'cc';
+const { ccclass, property, requireComponent, menu, executionOrder } = _decorator;
 
 
 @ccclass('NewComponent')
@@ -7,31 +7,39 @@ const { ccclass, property ,requireComponent,menu,executionOrder} = _decorator;
 @executionOrder(-1)
 export class NewComponent extends Component {
 
-    @property({type:CCInteger,tooltip:"sskk",displayName:"wok"})
-    age:number=3;
+    @property({ type: CCInteger, tooltip: "sskk", displayName: "wok" })
+    age: number = 3;
 
-    kk=Component;
+    kk = Component;
 
-    speed=3;
+    speed = 3;
     start() {
+        /*
+        let k=[3,2,1];
+        let k2={a:3,b:true}
+        console.log(...k);
+        */
+        /*
         console.log("normal start");
-            this.node.removeComponent(this);
-
-        //this.node.destroy();
-
-        let ani =this.node.getComponent(Animation);
+        this.node.removeComponent(this);
+        let ani = this.node.getComponent(Animation);
         ani.play("animation");
+        */
     }
 
     update(deltaTime: number) {
-        let pos:Vec3 =this.node.position;
-        pos.y+=this.speed;
+        let pos: Vec3 = this.node.position;
+        pos.y += this.speed;
 
-        this.node.position=pos;
+        this.node.position = pos;
     }
 
     protected onDestroy(): void {
         console.log("des")
+    }
+
+    click(e,data){
+        console.log("clicked !!!!:"+data);
     }
 }
 
